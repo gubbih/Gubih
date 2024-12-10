41fc61376dd5fe43edd7309f9a396c96c6b7638e
Thor <thorlhansen@hotmail.com>
2024-12-10 22:41:47 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SkillsComponent, HeaderComponent,ProjectsComponent,ExperienceComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Portfolio';
+}
